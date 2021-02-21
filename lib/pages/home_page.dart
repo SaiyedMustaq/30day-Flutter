dc4ed9f68f73a013bd205265acc0113678b9efa1
SAM <saiyedmscit16@gmial.com>
2021-02-21 10:35:18 +0530

    local json to load string data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pavan_kumar_flutter_30_days/model/Catlog.dart';
 import 'package:pavan_kumar_flutter_30_days/widget/ProductWidget.dart';
 import 'package:pavan_kumar_flutter_30_days/widget/drawer.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catlogJSON = await rootBundle.loadString('assets/files/catlog.json');
+    print(catlogJSON);
+    var decodeJsonr = jsonDecode(catlogJSON);
+    print(decodeJsonr);
+    var pdroductsData = decodeJsonr['product'];
+  }
+
   @override
   Widget build(BuildContext contgitext) {
     return Scaffold(
